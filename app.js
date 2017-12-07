3de413408c54985576b91cadc83eac818a3c5ac8
Fowder <emile.troccaz@hotmail.com>
2017-12-07 16:57:48 +0100

    Résolution exos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('have fun !');
+function afficher(){
+	$('.button').click(function (){
+		var first = $('#first_name').val();
+		var last = $('#last_name').val();
+		var city = $('#city').val();
+		$('#username').html(first +' '+last+' qui habite à '+city+'.');
+})
+}
+
+$(document).ready(function(){
+	afficher();
+});
